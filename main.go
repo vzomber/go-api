4f2fafdb754593cdd25355b61fd1d3a2c6318504
vzomber <34099961+vzomber@users.noreply.github.com>
2026-05-25 18:28:32 +0300

    first server

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type HealthResponse struct {
+	Status string `json:"status"`
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(HealthResponse{Status: "ok"})
+	if err != nil {
+		fmt.Println("Error encoding response:", err)
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/healthz", healthHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello, World!")
+
+		if err != nil {
+			fmt.Println("Error writing response:", err)
+		}
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Server error:", err)
+	}
+}
